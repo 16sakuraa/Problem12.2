4f724f95c5f0d9834f051d2f744c9bd2bac3677f
OAT <64010860@kmitl.ac.th>
2021-08-12 23:14:33 +0700

    several bugs fixed

--- Problem12.2/method1.2.cpp
+++ Problem12.2/method1.2.cpp
@@ -6,13 +6,13 @@
 int main()
 {
 	int a[3], max = -1000000, i, j;
-	scanf_s("%d %d %d", a[0], a[1], a[2]);
+	scanf_s("%d %d %d",&a[0],&a[1],&a[2]);
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			if (a[i] + a[j] >= max && a[i] != a[j])
+			if (a[i] + a[j] >= max && i!=j)
 			{
 				max = a[i] + a[j];
 			}
